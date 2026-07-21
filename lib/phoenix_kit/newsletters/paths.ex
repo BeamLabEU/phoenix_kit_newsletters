739f8a99c249dd45744309e1f623f0d7bbca18af
timujeen <timujeen@gmail.com>
2026-07-21 20:07:47 +0000

    Remove dangling lists path helpers (route targets removed with the list UI)

--- lib/phoenix_kit/newsletters/paths.ex
+++ lib/phoenix_kit/newsletters/paths.ex
@@ -11,12 +11,6 @@ defmodule PhoenixKit.Newsletters.Paths do
   def broadcast_edit(id), do: Routes.path("#{@base}/broadcasts/#{id}/edit")
   def broadcast_show(id), do: Routes.path("#{@base}/broadcasts/#{id}")
 
-  # Lists
-  def lists_index, do: Routes.path("#{@base}/lists")
-  def list_new, do: Routes.path("#{@base}/lists/new")
-  def list_edit(id), do: Routes.path("#{@base}/lists/#{id}/edit")
-  def list_members(id), do: Routes.path("#{@base}/lists/#{id}/members")
-
   # Public
   def unsubscribe, do: Routes.path("/newsletters/unsubscribe")
 end
